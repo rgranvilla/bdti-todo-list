20e5c6cbc548844fb202fba67fb6eaa7796d173f
Ricardo Granvilla <rgranvilla@gmail.com>
2021-09-02 16:16:47 -0300

    colors variables created

--- src/styles/global.js
+++ src/styles/global.js
@@ -9,6 +9,25 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  #root {
+    /* Principals Colors */
+    --primary: #0B4576;
+    --secondary: #EA7025;
+    --active: #29A30A;
+    --alert: #CA322D;
+    --unselected: #E7F2F1;
+
+    /* Background Colors */
+    --bg-button: #E7F2F1;
+    --bg-dashboard: #FEFEFE;
+    --bg-list: #F5F9FA;
+
+    /* Text Colors */
+    --txt-content: #888888;
+    --txt-title: #555555;
+    --txt-white: #FFFFFF;
+  }
+
   html, body, #root {
     height: 100%;
   }
