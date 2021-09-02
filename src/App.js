c3228d4da7bfe24d64c479346a8226185e78d7ab
Ricardo Granvilla <rgranvilla@gmail.com>
2021-09-02 16:41:37 -0300

    dashboard component created

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 
 import Header from './components/Header';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <>
       <Header />
+      <Dashboard />
 
       <GlobalStyle />
     </>
---
+++ src/components/Dashboard/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: calc(100% - 5rem);
+  border-radius: 10px;
+
+  background: var(--bg-dashboard);
+`;
