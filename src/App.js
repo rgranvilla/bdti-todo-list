d128804907825097d4b468aabf3d0a389d2ac069
Ricardo Granvilla <rgranvilla@gmail.com>
2021-09-02 16:00:15 -0300

    global styles implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import './App.css';
+
+import GlobalStyle from './styles/global';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Hello World!</h1>
+
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
